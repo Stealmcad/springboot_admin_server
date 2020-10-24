ddaac812e99bfff542f5e5ba6d5dcb284057af9c
Stealmcad <1142097441@qq.com>
2020-10-24 15:32:20 +0800

    增加一个新的文件

---
+++ src/main/java/com/atguigu/pojo/aaa.java
@@ -0,0 +1,17 @@
+package com.atguigu.pojo;
+
+public class aaa {
+
+    public static void main(String[] args) {
+        System.out.println("分支");
+        System.out.println("分支");
+        System.out.println("分支");
+        System.out.println("分支");
+        System.out.println("分支");
+        System.out.println("分支");
+        System.out.println("分支");
+        System.out.println("分支");
+        System.out.println("分支");
+    }
+
+}
