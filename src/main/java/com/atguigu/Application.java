96c929dbd5863e28e55feaf30f2e5bf07d2981e8
Stealmcad <1142097441@qq.com>
2020-10-24 15:27:40 +0800

    在IDEA中练习git使用

---
+++ src/main/java/com/atguigu/Application.java
@@ -0,0 +1,15 @@
+package com.atguigu;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer //启用Spring Boot Admin组件功能，进行对应用的监控，显示监控信息
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+}
